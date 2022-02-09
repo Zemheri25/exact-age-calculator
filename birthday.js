ec8ba4128d0dd4545d6e9f9387ee4138ded6d345
Zemheri25 <omerzemheri14@gmail.com>
2022-02-09 20:54:08 +0300

    exact age calculator project added

--- birthday.js
+++ birthday.js
@@ -15,7 +15,8 @@ let loading = document.getElementById("loading");
 
 
 
-input.addEventListener("change", () => {
+input.addEventListener("change", function starttime() {
+    
     countdown.style.display = "flex"
     let calcyear = new Date().getFullYear() - new Date(input.value).getFullYear();
     let calcMonths = new Date().getMonth() - new Date(input.value).getMonth();
@@ -24,6 +25,16 @@ input.addEventListener("change", () => {
     let calcMinutes = new Date().getMinutes() - new Date(input.value).getMinutes();
     let calcSeconds = new Date().getSeconds() - new Date(input.value).getSeconds();
     
+    if(calcMonths <0) {
+        calcMonths += 12;
+        calcyear -- 
+    }
+
+    if(calcDays < 0) {
+        calcDays += 30;
+        calcMonths --
+    }
+    
     years.innerHTML = calcyear;
     months.innerHTML = calcMonths;
     days.innerHTML = calcDays;
@@ -31,30 +42,10 @@ input.addEventListener("change", () => {
     minutes.innerHTML = calcMinutes;
     seconds.innerHTML = calcSeconds
 
-    if(calcDays < 0) {
-       days.innerHTML = calcDays + 31;
-       months.innerHTML = calcMonths + 11;
-       years.innerHTML = calcyear - 1;
-        if(calcMonths < 0) {
-            years.innerHTML = calcyear - 1;
-            months.innerHTML = calcMonths + 12;
-
-        }
-        
-    } else {
-       days.innerHTML = calcDays;
-       months.innerHTML = calcMonths;
-       years.innerHTML = calcyear;
-        if(calcMonths < 0) {
-            years.innerHTML = calcyear - 1;
-            months.innerHTML = calcMonths + 12;
-        
-    }
-   
-    }
+    setInterval(starttime, 1000)
     
     loading.style.visibility = "hidden"
-
+    body.style.backgroundİmage = "url(./images/background.jpg)"
     
 
     
